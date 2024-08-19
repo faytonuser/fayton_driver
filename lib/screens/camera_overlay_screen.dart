9714d8c10618d142154128ea69257f79ffe4769d
faytonuser <faytonuser@gmail.com>
2024-08-19 08:44:28 +0400

    Some bug fixed and versions updated

--- lib/screens/camera_overlay_screen.dart
+++ lib/screens/camera_overlay_screen.dart
@@ -1,13 +1,5 @@
-import 'dart:io';
-
-import 'package:camera/camera.dart';
-import 'package:driver/common/waiting_indicator.dart';
 import 'package:driver/providers/auth_provider.dart';
-import 'package:driver/screens/verification_waiting_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_camera_overlay/flutter_camera_overlay.dart';
-import 'package:flutter_camera_overlay/model.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 class CameraOverlayScreen extends StatefulWidget {
@@ -19,7 +11,7 @@ class CameraOverlayScreen extends StatefulWidget {
 }
 
 class _CameraOverlayScreenState extends State<CameraOverlayScreen> {
-  OverlayFormat format = OverlayFormat.cardID1;
+  // OverlayFormat format = OverlayFormat.cardID1;
   int tab = 0;
   @override
   void initState() {
@@ -30,35 +22,36 @@ class _CameraOverlayScreenState extends State<CameraOverlayScreen> {
   Widget build(BuildContext context) {
     var authProvider = Provider.of<AuthProvider>(context);
     return Scaffold(
-      body: FutureBuilder<List<CameraDescription>>(
-          future: availableCameras(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CustomWaitingIndicator(),
-              );
-            } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-              return CameraOverlay(
-                  snapshot.data!.first, CardOverlay.byFormat(format),
-                  (XFile file) {
-                if (widget.isFront) {
-                  print(file.path);
-                  authProvider.fronDriverLicense = File(file.path);
-                } else {
-                  authProvider.backDriverLicense = File(file.path);
-                }
-
-                Navigator.pop(context);
-              },
-                  info:
-                      'Şəxsiyyət vəsiqənizi düzbucaqlı içərisinə yerləşdirin və şəklin mükəmməl oxuna biləcəyinə əmin olun.',
-                  label: 'Şəxsiyyət vəsiqəsi skan edilir');
-            } else {
-              return Center(
-                child: Text("An error occured"),
-              );
-            }
-          }),
-    );
+        // flutter_camera_overlay
+        // body: FutureBuilder<List<CameraDescription>>(
+        //     future: availableCameras(),
+        //     builder: (context, snapshot) {
+        //       if (snapshot.connectionState == ConnectionState.waiting) {
+        //         return Center(
+        //           child: CustomWaitingIndicator(),
+        //         );
+        //       } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+        //         return CameraOverlay(
+        //             snapshot.data!.first, CardOverlay.byFormat(format),
+        //             (XFile file) {
+        //           if (widget.isFront) {
+        //             print(file.path);
+        //             authProvider.fronDriverLicense = File(file.path);
+        //           } else {
+        //             authProvider.backDriverLicense = File(file.path);
+        //           }
+        //
+        //           Navigator.pop(context);
+        //         },
+        //             info:
+        //                 'Şəxsiyyət vəsiqənizi düzbucaqlı içərisinə yerləşdirin və şəklin mükəmməl oxuna biləcəyinə əmin olun.',
+        //             label: 'Şəxsiyyət vəsiqəsi skan edilir');
+        //       } else {
+        //         return Center(
+        //           child: Text("An error occured"),
+        //         );
+        //       }
+        //     }),
+        );
   }
 }
--- lib/screens/face_overlay_screen.dart
+++ lib/screens/face_overlay_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:driver/providers/auth_provider.dart';
-import 'package:face_camera/face_camera.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,21 +9,22 @@ class FaceOverlayScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var authProvider = Provider.of<AuthProvider>(context);
     return Scaffold(
-      body: SmartFaceCamera(
-        autoCapture: true,
-        defaultCameraLens: CameraLens.front,
-        message: 'Center your face in the square',
-        onCapture: (File? image) {
-          authProvider.facePhoto = image;
-          if (image != null) {
-            authProvider.lastStep = true;
-            authProvider.facePhoto = image;
-            Navigator.pop(context, true);
-          } else {
-            Navigator.pop(context, false);
-          }
-        },
-      ),
-    );
+        //flutter_camera_overlay
+        // body: SmartFaceCamera(
+        //   autoCapture: true,
+        //   defaultCameraLens: CameraLens.front,
+        //   message: 'Center your face in the square',
+        //   onCapture: (File? image) {
+        //     authProvider.facePhoto = image;
+        //     if (image != null) {
+        //       authProvider.lastStep = true;
+        //       authProvider.facePhoto = image;
+        //       Navigator.pop(context, true);
+        //     } else {
+        //       Navigator.pop(context, false);
+        //     }
+        //   },
+        // ),
+        );
   }
 }
